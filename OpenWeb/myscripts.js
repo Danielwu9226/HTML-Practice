d17af7b2df6f1a96d6083ceff9ac62a3a8399b75
DanielDabouu9226 <Danielwu9226@gmail.com>
2023-08-29 11:04:26 -0400

    REMOVE FOLDER

--- OpenWeb/myscripts.js
+++
@@ -1,9 +0,0 @@
-const sections = document.querySelectorAll('section')
-
-document.addEventListener('scroll', () => {
-  sections.forEach(section => {
-    if(section.getBoundingClientRect().top <= document.body.scrollTop) {
-      document.body.style.background = section.dataset.color
-    }    
-  })
-})
